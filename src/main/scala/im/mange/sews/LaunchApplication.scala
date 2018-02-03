d45c540bb31b5811b1590b6e81ca9f254248d75b
pall <paulallton@gmail.com>
2018-02-03 12:11:05 +0000

    rename for reality

--- src/main/scala/im/mange/sews/LaunchApplication.scala
+++ src/main/scala/im/mange/sews/LaunchApplication.scala
@@ -3,7 +3,7 @@ package im.mange.sews
 import im.mange.sews.innards._
 import io.shaka.http.Http.HttpHandler
 
-object Application {
+object LaunchApplication {
   def apply(port: Int, config: Config): Unit = {
     WebServer(port,
       Handler("/ws/*", JettyWebSocketHandler(config.program)),
