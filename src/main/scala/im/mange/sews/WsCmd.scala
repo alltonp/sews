98a64c001859b8f522ccad1f76cfbfe14d12a613
pall <paulallton@gmail.com>
2018-02-21 16:37:04 +0000

    send takes option to keep client code cleaner

--- src/main/scala/im/mange/sews/WsCmd.scala
+++ src/main/scala/im/mange/sews/WsCmd.scala
@@ -1,7 +1,7 @@
 package im.mange.sews
 
 case class WsCmd[IN, OUT](codec: JsonCodec[IN, OUT], all: Subscribers) {
-  def send(msg: OUT, to: Subscriber): Cmd = () => { all.send(jsonise(msg), to) }
+  def send(msg: OUT, to: Option[Subscriber]): Cmd = () => { to.foreach(all.send(jsonise(msg), _)) }
   def sendAll(msg: OUT): Cmd = () => { all.sendAll(jsonise(msg)) }
 
   private def jsonise(msg: OUT) = codec.encode(msg)
