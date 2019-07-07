c54a2ef7dedf71e2a3fa773940b1465e2bdf9f7f
paulallton@gmail.com <goPfYgofPDD4teFYj>
2019-07-07 18:24:03 +0100

    try to default subscribers

--- src/main/scala/im/mange/sews/Update.scala
+++ src/main/scala/im/mange/sews/Update.scala
@@ -2,7 +2,7 @@ package im.mange.sews
 
 trait Update[IN, MODEL, OUT] {
   val msgCodec: JsonCodec[IN, OUT]
-  val subscribers: Subscribers
+  val subscribers: Subscribers = Subscribers(Nil)
 
   def update(msg: IN, model: MODEL, from: Option[Subscriber]): (MODEL, Cmd)
 }
